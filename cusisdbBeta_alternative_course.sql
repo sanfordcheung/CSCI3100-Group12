09f9b17536eed71afba62969fc06b642967ed4b7
Sqzlnsy <62003760+Sqzlnsy@users.noreply.github.com>
2020-04-20 20:29:33 +0800

    Add files via upload
    相互可替代的课程

---
+++ cusisdbBeta_alternative_course.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: cusisdbBeta
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alternative_course`
+--
+
+DROP TABLE IF EXISTS `alternative_course`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alternative_course` (
+  `id` int NOT NULL,
+  `course id` varchar(45) NOT NULL,
+  PRIMARY KEY (`course id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alternative_course`
+--
+
+LOCK TABLES `alternative_course` WRITE;
+/*!40000 ALTER TABLE `alternative_course` DISABLE KEYS */;
+INSERT INTO `alternative_course` VALUES (1,'ENGG1110'),(2,'ENGG1310'),(3,'ENGG1410'),(4,'ENGG2430'),(5,'ENGG2440'),(1,'ESTR1002'),(2,'ESTR1003'),(3,'ESTR1004'),(4,'ESTR2002'),(5,'ESTR2004'),(6,'UGFH1000'),(6,'UGFH1001'),(7,'UGFN1000'),(7,'UGFN1001');
+/*!40000 ALTER TABLE `alternative_course` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-20 20:28:05
