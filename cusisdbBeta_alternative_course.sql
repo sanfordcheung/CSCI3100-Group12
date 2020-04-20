c6da640e81b2a9f6ac13b181fbca238a4677375d
Sqzlnsy <62003760+Sqzlnsy@users.noreply.github.com>
2020-04-20 20:32:32 +0800

    Update cusisdbBeta_alternative_course.sql

--- cusisdbBeta_alternative_course.sql
+++ cusisdbBeta_alternative_course.sql
@@ -3,6 +3,7 @@
 -- Host: localhost    Database: cusisdbBeta
 -- ------------------------------------------------------
 -- Server version	8.0.19
+use cusisdbBeta;
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
