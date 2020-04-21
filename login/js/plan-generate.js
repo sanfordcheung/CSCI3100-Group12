5149018d43685faa06f0229575f231c05a43a926
sanfordcheung <sanfordcheung@gmail.com>
2020-04-22 00:33:46 +0800

    update page

--- login/js/plan-generate.js
+++ login/js/plan-generate.js
@@ -1,22 +1,25 @@
 $(document).ready(function() {
-    $('#sidEnter').click(function () {
-        var word = document.getElementById("sidInput").value;
-        $.ajax('/shoppingCart', {
+    $('#getPlan').click(function () {
+        var table = document.getElementById("shoppingCartBody");
+        var innerData = [];
+        for (var i=0;i<table.rows.length;i++) {
+            var rowData = {};
+            var r = table.rows[i].innerHTML.split("</td>");
+            rowData["course_id"] = r[0].slice(4);
+            rowData["session_id"] = r[1].slice(4);
+            rowData["credit"] = r[2].slice(4);
+            rowData["preference"] = i;
+            innerData.push(rowData);
+        }
+
+        $.ajax('/generatePlan', {
             type: 'POST',
-            data: {keyword: word}
+            data: {courseData: innerData}
         }).done(function (response) {
-            $('#shoppingCartBody').empty();
-            for (var i=0;i<response.courseData.length;i++) {
-                var tr = $('<tr></tr>');
-                for (var str in response.courseData[i]) {
-                    var td = $('<td></td>').text(response.courseData[i][str]);
-                    tr.append(td);
-                }
-                $('#shoppingCartBody').append(tr);
-            }
+            alert("generate plan done");
         }) ;
 
-    })
+    });
 });
 
 $( function() {
